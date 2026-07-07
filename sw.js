74300baf5fadfa90650583cb4fe57eff637a7bce
Arjona-plusAI <161945614+Arjona-plusAI@users.noreply.github.com>
2026-07-07 18:06:33 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -61,3 +61,29 @@ self.addEventListener('fetch', function(e) {
         })
     );
 });
+/* Auto activate new version */
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open(CACHE).then(function(c) {
+            return c.addAll(FILES);
+        }).then(function() {
+            return self.skipWaiting();
+        })
+    );
+});
+
+self.addEventListener('activate', function(e) {
+    e.waitUntil(
+        caches.keys().then(function(keys) {
+            return Promise.all(
+                keys.filter(function(k) {
+                    return k !== CACHE;
+                }).map(function(k) {
+                    return caches.delete(k);
+                })
+            );
+        }).then(function() {
+            return self.clients.claim();
+        })
+    );
+});
