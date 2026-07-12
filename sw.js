fc3b06a4f3ed6b1b974c19c938063f72a1015d6b
Arjona-plusAI <161945614+Arjona-plusAI@users.noreply.github.com>
2026-07-12 16:54:59 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
    Cleaned: duplicate listeners removed, artifacts removed
    ============================================ */
 
-var CACHE = 'arjona-v8';
+var CACHE = 'arjona-v9';
 var FILES = [
     '/',
     '/index.html',
